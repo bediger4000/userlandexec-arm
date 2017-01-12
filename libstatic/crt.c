eb9aeacba0d4a624821d509c3c29237c8c160114
Bruce Ediger <bediger8@gmail.com>
2017-01-12 18:23:32 +0000

    Fix shift bits so that envp gets populated correctly.

--- libstatic/crt.c
+++ libstatic/crt.c
@@ -6,7 +6,7 @@ asm(
 "       sub     lr, lr, lr\n"           // Clear the link register.
 "       ldr     r0, [sp]\n"             // Get argc...
 "       add     r1, sp, #4\n"           // ... and argv ...
-"       add     r2, r1, r0, LSL #2\n"   // ... and compute environ.
+"       add     r2, r1, r0, LSL #3\n"   // ... and compute environ.
 "       bl      c_main\n"              // Let's go!
 "       b       .\n"                    // Never gets here.
 "       .size   _start, .-_start\n"
