dec474c982a4e325becdb1f583fd338bbfa33f02
Bruce Ediger <bediger8@gmail.com>
2017-01-12 05:25:49 +0000

    Get working, albeit slow, division and modulo functions

--- libstatic/idiv0.s
+++ libstatic/idiv0.s
@@ -1,17 +1,43 @@
-@ Software simulation of 32 bit unsigned integer division
-@ Entry  r0: numerator (lo) must be signed positive
-@      r6: denominator (den) must be non-zero and signed negative
-@ int idiv0(long numerator, long denominator)
-
-      lo .req r0; hi .req r5; den .req r6
-.globl idiv0
-.type idiv0, %function
-idiv0:
-mov %r4, #0 @ hi = 0
-adds %r5, %r5, %r5
-.rept 32 @ repeat 32 times
-	adcs %r4, %r6, %r4, lsl #1
-	subcc %r4, %r4, %r6
-	adcs %r5, %r5, %r5
-.endr
-bx %lr
+.globl unsigned_naive_div
+.type unsigned_naive_div, %function
+unsigned_naive_div:
+@/* r0 contains N */
+@/* r1 contains D */
+    mov %r2, %r1             @/* r2 ← r0. We keep D in r2 */
+    mov %r1, %r0             @/* r1 ← r0. We keep N in r1 */
+ 
+    mov %r0, #0             @/* r0 ← 0. Set Q = 0 initially */
+ 
+    b .Lloop_check2
+    .Lloop2:
+       add %r0, %r0, #1      @/* r0 ← r0 + 1. Q = Q + 1 */
+       sub %r1, %r1, %r2      @/* r1 ← r1 - r2 */
+    .Lloop_check2:
+       cmp %r1, %r2          @/* compute r1 - r2 */
+       bhs .Lloop2          @/* branch if r1 >= r2 (C=0 or Z=1) */
+ 
+    @/* r0 already contains Q */
+    @/* r1 already contains R */
+    bx lr
+.globl unsigned_naive_mod
+.type unsigned_naive_mod, %function
+unsigned_naive_mod:
+@/* r0 contains N */
+@/* r1 contains D */
+    mov %r2, %r1             @/* r2 ← r0. We keep D in r2 */
+    mov %r1, %r0             @/* r1 ← r0. We keep N in r1 */
+ 
+    mov %r0, #0             @/* r0 ← 0. Set Q = 0 initially */
+ 
+    b .Lloop_check
+    .Lloop:
+       add %r0, %r0, #1      @/* r0 ← r0 + 1. Q = Q + 1 */
+       sub %r1, %r1, %r2      @/* r1 ← r1 - r2 */
+    .Lloop_check:
+       cmp %r1, %r2          @/* compute r1 - r2 */
+       bhs .Lloop          @/* branch if r1 >= r2 (C=0 or Z=1) */
+ 
+    @/* r0 already contains Q */
+    @/* r1 already contains R */
+    mov %r0, %r1
+    bx lr
