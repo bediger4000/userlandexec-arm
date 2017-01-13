cb2441471fa1e14cd2ec55ed8dd7096f4c0cbbcb
Bruce Ediger <bediger8@gmail.com>
2017-01-13 17:28:02 +0000

    Fix a misunderstaning about callee saves %r4

--- set_errno.s
+++ set_errno.s
@@ -1,8 +1,8 @@
 .globl set_errno
 .type set_errno, %function
 set_errno:
-movw    %r4, #:lower16:errno
-movt    %r4, #:upper16:errno
-str     %r0, [%r4]
+movw    %r3, #:lower16:errno
+movt    %r3, #:upper16:errno
+str     %r0, [%r3]
 add %r0, %r0, #1
 bx %lr
