d68960a5eff718a01b81590fe0ac14e228fb2061
Bruce Ediger <bediger8@gmail.com>
2017-01-13 17:31:01 +0000

    Fix up a problem with kernel returning negative number on error, but branching back to calling code anyway. Systematic, due to misunderstanding.

--- libstatic/linux_munmap.s
+++ libstatic/linux_munmap.s
@@ -6,9 +6,10 @@ mov     %r7, $92
 swi     $0
 pop     {%r7}
 cmn     %r0, $4096
-bx      %lr 
+bxcc      %lr 
 movw    %r3, #:lower16:errno
 movt    %r3, #:upper16:errno
+neg %r0, %r0
 str     %r0, [%r3]
 mov     %r0, $-1
 bx %lr
--- libstatic/linux_read.s
+++ libstatic/linux_read.s
@@ -5,10 +5,11 @@ push    {%r7}
 mov     %r7, $3
 swi     $0
 pop     {%r7}
-cmn     %r0, $4096
-bx      %lr 
+cmp     %r0, $0
+bxcc      %lr 
 movw    %r3, #:lower16:errno
 movt    %r3, #:upper16:errno
+neg %r0, %r0
 str     %r0, [%r3]
 mov     %r0, $-1
 bx %lr
