598a453cc0c3ca8090af731e5b8a79c9b213491f
Bruce Ediger <bediger8@gmail.com>
2017-01-14 13:01:32 +0000

    Name change.

---
+++ libstatic/linux_brk.s
@@ -0,0 +1,23 @@
+.globl linux_brk
+.type linux_brk, %function
+linux_brk:
+push    {%r7}
+mov     %r7, $45
+swi     $0
+pop     {%r7}
+cmn     %r0, $4096
+bxcc      %lr 
+ldr     %r2, .L3
+.LPIC0:
+add     %r2, %pc, %r2
+ldr  %r3, .L3+4
+ldr     %r3, [%r2, %r3]
+neg %r0, %r0
+str     %r0, [%r3]
+mov     %r0, $-1
+bx %lr
+.align  2
+.L3:
+.word   _GLOBAL_OFFSET_TABLE_-(.LPIC0+8)
+.word   errno(GOT)
+.size   linux_brk, .-linux_brk
