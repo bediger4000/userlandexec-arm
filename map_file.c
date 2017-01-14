c419ffedbbb5fad14db306892ee0f9c04235e248
Bruce Ediger <bediger8@gmail.com>
2017-01-14 13:06:58 +0000

    Remove stray debug code.

--- map_file.c
+++ map_file.c
@@ -17,9 +17,6 @@ map_file(char *file_to_map)
 		error_msg("map_file stat() failed ");
 		linux_exit(1);
 	}
-	print_string(1, "linux_stat file size ");
-	print_long(1, sz);
-	print_string(1, "\n");
 
 	mapped = linux_mmap(NULL, sz, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
 
