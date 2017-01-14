2c961b0bba072adbf42f3753d524211d21fd366b
Bruce Ediger <bediger8@gmail.com>
2017-01-14 00:55:05 +0000

    Add errno output to get a clue about failures.

--- mycat.c
+++ mycat.c
@@ -7,10 +7,25 @@ c_main(int ac, char **av, char **envp)
 	int cc;
 	char buf[BUFSIZ];
 	if (fd < 0) {
+		print_string(1, "open() failed: ");
+		print_long(1, errno);
+		print_string(1, "\n");
 		linux_exit(1);
 	}
+	errno = 0;
 	while (0 < (cc = linux_read(fd, buf, sizeof(buf))))
-		linux_write(1, buf, cc);
+		if (0 > linux_write(1, buf, cc)) {
+			print_string(1, "write() failed: ");
+			print_long(1, errno);
+			print_string(1, "\n");
+			linux_exit(1);
+		}
+	if (cc < 0) {
+		print_string(1, "read() failed: ");
+		print_long(1, errno);
+		print_string(1, "\n");
+		linux_exit(1);
+	}
 	linux_close(fd);
 	linux_exit(0);
 }
