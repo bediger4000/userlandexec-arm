df3660c1d38b7c8d84c3c5235ffa28ee7da99473
Bruce Ediger <bediger8@gmail.com>
2017-01-14 03:15:16 +0000

    Fix bug: (void *)sbuf instead of (void *)&sbuf, which is pointer to pointer

--- libstatic/file_size.c
+++ libstatic/file_size.c
@@ -6,7 +6,8 @@ file_size(char *filename)
 	char sbuf[144];
 	unsigned long ret;
 
-	if (0 > (long)(ret = linux_stat(filename, (void *)&sbuf)))
+	/* if (0 > (long)(ret = linux_stat(filename, (void *)&sbuf))) */
+	if (0 > (long)(ret = linux_stat(filename, (void *)sbuf)))
 	{
 		print_string(2, "stat problem: ");
 		print_long(2, errno);
