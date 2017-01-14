0a359642dd9762d9a367f43c75d765d12062ca87
Bruce Ediger <bediger8@gmail.com>
2017-01-14 00:55:54 +0000

    Change from position-dependent to position-idendepent setting of errno

--- libstatic/linux_open.s
+++ libstatic/linux_open.s
@@ -7,9 +7,17 @@ swi     $0
 pop     {%r7}
 cmn     %r0, $4096
 bxcc      %lr 
-movw    %r3, #:lower16:errno
-movt    %r3, #:upper16:errno
+ldr     %r2, .L3
+.LPIC0:
+add     %r2, %pc, %r2
+ldr  %r3, .L3+4
+ldr     %r3, [%r2, %r3]
 neg %r0, %r0
 str     %r0, [%r3]
 mov     %r0, $-1
 bx %lr
+.align  2
+.L3:
+.word   _GLOBAL_OFFSET_TABLE_-(.LPIC0+8)
+.word   errno(GOT)
+.size   linux_open, .-linux_open
